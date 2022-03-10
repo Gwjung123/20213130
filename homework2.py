1d11cbf2c49b3c1bfc110ba1f5d5f7a1013d732c
20213130-정권우 <100751659+Gwjung123@users.noreply.github.com>
2022-03-10 22:21:00 +0900

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,15 @@
+a,b,c,d,e = map(int,input().split());
+
+print(a+b+c+d+e);
+
+print(max(a,b,c,d,e));
+
+print(min(a,b,c,d,e));
+
+print((a+b+c+d+e) / 5);
+
+
+
+
+
+
---
+++ homework4.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+
+A = np.array([[3,0] 
+            , [-1,2] , 
+            [1,1]])
+
+B = np.array([[-3,-1] 
+            , [2,1] , 
+            [4,3]])
+
+C = np.array([[1,2,3] 
+            , [2,0,1]])
+
+D = np.array([[1] , 
+              [2] , 
+              [3]])
+
+print(A + 2*B)
+
+print(A * C)
+
+print(3 * D)
+
+
+
+
